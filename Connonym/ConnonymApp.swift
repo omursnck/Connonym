14cec716460c6bb7e4b321dc53db6eeb6215a583
omursnck <omrsnr12@icloud.com>
2023-12-17 20:57:49 +0300

    Initial Commit

---
+++ Connonym/ConnonymApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConnonymApp.swift
+//  Connonym
+//
+//  Created by Ömür Şenocak on 17.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct ConnonymApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
